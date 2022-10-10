3707d1007d0eaa914cee1d29de53e360c8db3092
GanonUchiha <jameszuchen@gmail.com>
2022-10-10 18:12:21 +0800

    Implemented full page archive

--- src/bahamut.py
+++ src/bahamut.py
@@ -27,96 +27,146 @@ class PostMetadata:
             link = self.link
         )
 
-def extract_post_header(post: Tag, original_link: str) -> PostMetadata:
-    '''
-    Extracts the header from a post.
+class BahamutPost:
+    URL_PREFIX = "https://forum.gamer.com.tw/"
+    SEPARATOR = "\n--------------------------------------\n"
 
-    ## Parameters:
-    post: `Tag`
-        A single post, in HTML format
+    def __init__(self, post: Tag, original_link: str = ""):
+        self.post: Tag = post
+        self.original_link: str = original_link
+        self.metadata: PostMetadata = None
+        self.content: str = ""
+        self.hashtags: List[str] = []
+
+        self.extract()
     
-    ## Returns:
-    `dict`
-        TBD
-    '''
-    global URL_PREFIX
-
-    post_header = post.find("div", attrs={"class": "c-post__header"})
-    try:
-        post_title = post.find("h1", attrs={"class": "c-post__header__title"}).text
-    except:
-        post_title = "No Title"
-    post_floor = post_header.find("a", attrs={"class": "tippy-gpbp"}).attrs["data-floor"]
-    post_href = post_header.find("a", attrs={"class": "tippy-gpbp"}).attrs["href"]
-    post_username = post_header.find("a", attrs={"class": "username"}).text
-    post_userid = post_header.find("a", attrs={"class": "userid"}).text
-    # print("#{} by {}({})".format(post_floor, post_username, post_userid))
+    @property
+    def title(self):
+        try:
+            return self.metadata.title
+        except:
+            return ""
     
-    if post_href != "":
-        post_link = URL_PREFIX + post_href
-    else:
-        post_link = original_link
+    @title.setter
+    def title(self, new_title: str):
+        self.metadata.title = new_title
 
-    post_metadata = {
-        "title": post_title,
-        "floor": post_floor,
-        "username": post_username,
-        "userid": post_userid,
-        "link": post_link
-    }
-    return PostMetadata(**post_metadata)
-
-def extract_post_body(post: Tag) -> str:
-    '''
-    Extracts the main content from a post, and convert it into a pure text format.
-    All images, hyperlinks and embeds are replaced with a URL linking to the original content.
-
-    ## Parameters:
-    post: `Tag`
-        A single post, in HTML format
+    @property
+    def floor(self):
+        try:
+            return self.metadata.floor
+        except:
+            return "-1"
     
-    ## Returns:
-    `str`
-        Extracted content as a string, without tags
-    '''
-    post_body = post.find("div", attrs={"class": "c-article__content"})
+    def export(self, include_header: bool = False, include_hashtags: bool = False) -> str:
 
-    # Replace image Tag with image URL
-    for img in post_body.find_all("a", attrs={"class": "photoswipe-image"}):
-        img: Tag
-        img = img.replace_with(img.attrs["href"])
+        output = []
 
-    # Replace YouTube embeds with origininal video URL
-    for yt in post_body.find_all("div", attrs={"class": "video-youtube"}):
-        yt: Tag
-        yt_id = yt.find("iframe").attrs["data-src"].split("/")[-1].split("?")[0]
-        yt_link = "https://www.youtube.com/watch?v={}".format(yt_id)
-        yt = yt.replace_with(yt_link)
+        if include_header:
+            output.append(self.metadata.info)
+        output.append(self.content)
+        if include_hashtags:
+            output.append("Hashtags: {}".format(", ".join(self.hashtags)))
+        
+        return self.SEPARATOR.join(output)
 
-    # Replace hyperlink Tag with original URL
-    for link in post_body.find_all("a"):
-        link: Tag
-        real_link = unquote(link.attrs["href"]).split("?url=")[-1]
-        link = link.replace_with("{} (<{}>)".format(link.text, real_link))
-    # print(post_body)
+    def extract(self, extract_hashtags: bool = False):
+        
+        self.extract_post_header()
+        self.extract_post_body()
+        if extract_hashtags:
+            self.extract_hashtags_from_text()
 
-    # Replace br Tags with newlines
-    for br in post_body.find_all("br"):
-        br: Tag
-        br = br.replace_with("\n")
+    def extract_post_header(self):
+        '''
+        Extracts the header from a post.
 
-    # Replace div Tags and add newlines
-    for div in post_body.find_all("div"):
-        div: Tag
-        div = div.replace_with(div.text.strip()+"\n")
-    # print(post_body)
+        ## Parameters:
+        post: `Tag`
+            A single post, in HTML format
+        original_link: `str`
+            The original link of the webpage
+        '''
 
-    return post_body.text
+        post_header = self.post.find("div", attrs={"class": "c-post__header"})
+        try:
+            post_title = self.post.find("h1", attrs={"class": "c-post__header__title"}).text
+        except:
+            post_title = "No Title"
+        post_floor = post_header.find("a", attrs={"class": "tippy-gpbp"}).attrs["data-floor"]
+        post_href = post_header.find("a", attrs={"class": "tippy-gpbp"}).attrs["href"]
+        post_username = post_header.find("a", attrs={"class": "username"}).text
+        post_userid = post_header.find("a", attrs={"class": "userid"}).text
+        # print("#{} by {}({})".format(post_floor, post_username, post_userid))
+        
+        if post_href != "":
+            post_link = self.URL_PREFIX + post_href
+        else:
+            post_link = self.original_link
 
-def extract_hashtags_from_text(text: str) -> List[str]:
-    hashtags = re.findall("(#)(\w+)(\Z|\W)", text)
-    # print(hashtags)
-    return [tag[1] for tag in hashtags]
+        post_metadata = {
+            "title": post_title,
+            "floor": post_floor,
+            "username": post_username,
+            "userid": post_userid,
+            "link": post_link
+        }
+        self.metadata = PostMetadata(**post_metadata)
+
+    def extract_post_body(self):
+        '''
+        Extracts the main content from a post, and convert it into a pure text format.
+        All images, hyperlinks and embeds are replaced with a URL linking to the original content.
+
+        ## Parameters:
+        post: `Tag`
+            A single post, in HTML format
+        '''
+        post_body = self.post.find("div", attrs={"class": "c-article__content"})
+
+        # Replace image Tag with image URL
+        for img in post_body.find_all("a", attrs={"class": "photoswipe-image"}):
+            img: Tag
+            img = img.replace_with(img.attrs["href"])
+
+        # Replace YouTube embeds with origininal video URL
+        for yt in post_body.find_all("div", attrs={"class": "video-youtube"}):
+            yt: Tag
+            yt_id = yt.find("iframe").attrs["data-src"].split("/")[-1].split("?")[0]
+            yt_link = "https://www.youtube.com/watch?v={}".format(yt_id)
+            yt = yt.replace_with(yt_link)
+
+        # Replace Twitch embeds with origininal channel URL
+        for tw in post_body.find_all("div", attrs={"class": "video-twitchvod"}):
+            tw: Tag
+            tw_id = tw.find("iframe").attrs["data-src"].split("&")[0].split("=")[-1]
+            tw_link = f"https://www.twitch.tv/{tw_id}"
+            tw = tw.replace_with(tw_link)
+
+        # Replace hyperlink Tag with original URL
+        for link in post_body.find_all("a"):
+            link: Tag
+            real_link = unquote(link.attrs["href"]).split("?url=")[-1]
+            link = link.replace_with("{} (<{}>)".format(link.text, real_link))
+        # print(post_body)
+
+        # Replace br Tags with newlines
+        for br in post_body.find_all("br"):
+            br: Tag
+            br = br.replace_with("\n")
+
+        # Replace div Tags and add newlines
+        for div in post_body.find_all("div"):
+            div: Tag
+            div = div.replace_with(div.text.strip()+"\n")
+        # print(post_body)
+
+        self.content = post_body.text
+
+    def extract_hashtags_from_text(self):
+        hashtags = re.findall("(#)(\w+)(\Z|\W)", self.content)
+        # print(hashtags)
+        self.hashtags = [tag[1] for tag in hashtags]
 
 def get_posts(soup: BeautifulSoup) -> List[Tag]:
     sections: List[Tag] = soup.find_all("section", attrs={"class": "c-section"})
@@ -141,17 +191,20 @@ def main():
     soup = BeautifulSoup(response.text, features="lxml")
     posts: List[Tag] = get_posts(soup)
     # print([post.attrs for post in posts])
+    post: BahamutPost = BahamutPost(posts[0], test_link)
+    post.extract(extract_hashtags=True)
     
-    post_metadata: PostMetadata = extract_post_header(posts[0], test_link)
-    post_body_text: str = extract_post_body(posts[0]).strip()
-    post_hashtags: List[str] = extract_hashtags_from_text(post_body_text)
+    # post_metadata: PostMetadata = extract_post_header(posts[0], test_link)
+    # post_body_text: str = extract_post_body(posts[0]).strip()
+    # post_hashtags: List[str] = extract_hashtags_from_text(post_body_text)
     
     with open("cleaned.txt", "w", encoding="utf8") as fp:
-        fp.write(post_metadata.info)
-        fp.write("\n-----------------------------------------\n")
-        fp.write(post_body_text)
-        fp.write("\n-----------------------------------------\n")
-        fp.write("Hashtags: {}".format(", ".join(post_hashtags)))
+        fp.write(post.export(include_header=True, include_hashtags=True))
+        # fp.write(post_metadata.info)
+        # fp.write("\n-----------------------------------------\n")
+        # fp.write(post_body_text)
+        # fp.write("\n-----------------------------------------\n")
+        # fp.write("Hashtags: {}".format(", ".join(post_hashtags)))
 
 if __name__ == "__main__":
     main()
--- src/bahamutarchiver.py
+++ src/bahamutarchiver.py
@@ -7,13 +7,13 @@ from bs4 import Tag, BeautifulSoup
 
 import discord
 from discord.ext import commands
-from discord import ForumTag, TextStyle, Webhook, app_commands, AppCommandType, Interaction, ButtonStyle
+from discord import InteractionResponded, app_commands, Interaction, Thread
 from discord.ext.commands import Bot
 from discord.app_commands import CommandTree, ContextMenu, Command, Group, describe, Choice
 from discord.channel import ForumChannel
 
 from settings import SharedVariables
-from bahamut import get_webpage, get_posts, extract_post_header, extract_post_body, PostMetadata
+from bahamut import BahamutPost, get_webpage, get_posts
 
 class ChannelDropdown(discord.ui.Select):
 
@@ -42,42 +42,101 @@ class BahamutAchiver(commands.Cog):
     
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
+        self.selected_channel: ForumChannel = None
     
     
-    @app_commands.command(name="bahamut-archive", description="Archive a post at bahamut as a DC forum post")
+    @app_commands.command(name="bh-archive", description="Archive a post at bahamut as a DC forum post")
     @app_commands.choices(archive_range=[
-        Choice(name="Main Post", value=1),
-        Choice(name="First Page", value=2),
-        Choice(name="Entire Thread", value=3),
+        Choice(name="First Post", value=1),
+        Choice(name="Page", value=2),
+        Choice(name="Thread", value=3),
     ])
     @app_commands.rename(archive_range="range", post_url="url")
-    async def create_forum_thread_interactive(self, interaction: Interaction, archive_range: Choice[int], post_url: str):
+    async def bahamut_archive(self, interaction: Interaction, archive_range: Choice[int], post_url: str):
         '''
         Archive a post at bahamut as a DC forum post
         '''
-        print("[LOG] User {} wants to archive a post with range {}.".format(interaction.user, archive_range))
-        await interaction.response.defer(ephemeral=True, thinking=True)
+        print("[LOG] User {} wants to archive a post with range {}.".format(interaction.user, archive_range.name))
+        await interaction.response.defer(ephemeral=True)
+        
+        # Ask user to select a channel if not selected before
+        if self.selected_channel == None:
+            all_channels = await interaction.guild.fetch_channels()
+            forum_channels = [channel for channel in all_channels if type(channel) is ForumChannel]
+            self.selected_channel = await self.ask_select_channel(interaction, forum_channels)
 
         response: requests.Response = get_webpage(post_url)
         soup = BeautifulSoup(response.text, features="lxml")
+        is_main_page = (soup.find("p", attrs={"class": "BH-pagebtnA"}) != None)
         posts: List[Tag] = get_posts(soup)
-        post_meta: PostMetadata = extract_post_header(posts[0], post_url)
-        post_title = post_meta.title
-        post_content = extract_post_body(posts[0])
         
-        match archive_range:
-            case 1:
-                pass
+        match archive_range.value:
+            case 1: # First Page
+                post = BahamutPost(posts[0], post_url)
+                thread: Thread =await self.archive_post(post)
+                
+                # Send final message
+                await interaction.followup.send(
+                    content="Thread created at {}: {}".format(self.selected_channel.name, thread.jump_url),
+                    ephemeral=False
+                )
+            case 2 | 3 if not is_main_page: # Main Post
+                created_threads = await self.archive_page(posts, post_url)
+                thread_urls = "\n".join([thr.jump_url for thr in created_threads])
+
+                # Send final message
+                await interaction.followup.send(
+                    content="Thread created at {}:\n{}".format(self.selected_channel.name, thread_urls),
+                    ephemeral=False
+                )
+            case 3: # Whole Thread
+                if soup.find("p", attrs={"class": "BH-pagebtnA"}):
+                    pass
+                else:
+                    created_threads = await self.archive_page(posts, post_url)
+                    thread_urls = "\n".join([thr.jump_url for thr in created_threads])
+
+                    # Send final message
+                    await interaction.followup.send(
+                        content="Thread created at {}:\n{}".format(self.selected_channel.name, thread_urls),
+                        ephemeral=False
+                    )
             case _:
-                pass
+                # Send message
+                await interaction.followup.send(
+                    content="Option {} not supported yet".format(archive_range.name), ephemeral=True
+                )
+
+    async def archive_page(self, posts_raw: List[Tag], page_url: str) -> List[Thread]:
+        created_threads: List[Thread] = []
+        thread_title = BahamutPost(posts_raw[0]).title
+        for post_raw in posts_raw:
+            post = BahamutPost(post_raw, page_url)
+            thread: Thread = await self.archive_post(post, thread_title=thread_title)
+            created_threads.append(thread)
         
+        return created_threads
+    
+    async def archive_post(self, post: BahamutPost, thread_title: str="") -> Thread:
+        if post.title == "No Title":
+            post.title = thread_title
+        post_content = post.export(include_header=True)
+        # Create the thread
+        thread, _ = await self.selected_channel.create_thread(
+            name=f"{post.title} \#{post.floor}",
+            content=post_content
+        )
+        return thread
+
+    @app_commands.command(name="bh-set-channel", description="Set default channel to send the archive to")
+    async def bh_select_channel(self, interaction: Interaction):
         all_channels = await interaction.guild.fetch_channels()
         forum_channels = [channel for channel in all_channels if type(channel) is ForumChannel]
-        selected_channel = await self.ask_select_channel(interaction, forum_channels)
-
-        # Create the thread
-        thread, _ = await selected_channel.create_thread(name=post_title, content=post_content)
-        await interaction.followup.send(content="Thread created at: {}".format(thread.jump_url), ephemeral=True)
+        self.selected_channel = await self.ask_select_channel(interaction, forum_channels)
+        await interaction.followup.send(
+            content="Forum channel set to: {}".format(self.selected_channel.name),
+            ephemeral=True
+        )
 
     async def ask_select_channel(self, interaction: Interaction, forum_channels: List[ForumChannel]) -> ForumChannel:
 
@@ -85,10 +144,17 @@ class BahamutAchiver(commands.Cog):
         view = SelectMessage()
         select_options = ChannelDropdown(forum_channels)
         view.add_components(select_options)
+        try:
+            await interaction.response.defer(ephemeral=True, thinking=True)
+        except InteractionResponded:
+            pass
         response1 = await interaction.followup.send(content="Create Thread in:", view=view, ephemeral=True, wait=True)
 
         # Wait until the user interacts with the dropdown box
-        await self.bot.wait_for('interaction', check=lambda interact: interact.data["custom_id"] == view.button_id)
+        await self.bot.wait_for(
+            'interaction',
+            check=lambda interact: "custom_id" in interact.data and interact.data["custom_id"] == view.button_id
+        )
         for item in view.children:
             # print(item.custom_id, res.data["custom_id"])
             if item.custom_id == view.dropdown_id:
